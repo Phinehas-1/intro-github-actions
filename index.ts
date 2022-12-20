a760f464e1bab1b1ed0ae00288cfa1a73b94ed58
Phinehas Enakireru <phinehasenakireru@gmail.com>
2022-12-20 12:18:59 +0100

    remove class definition for clock, replace with showtime function only.

--- index.ts
+++ index.ts
@@ -1,14 +1,12 @@
-class Clock {
-    clock_container = document.getElementById('clock-container');
-    date: Date = new Date();
-    showTime() {
-        let hours: number = this.date.getHours();
-        let minutes: number = this.date.getMinutes();
-        let seconds: number = this.date.getSeconds();
-        let time = document.createTextNode(`${hours} : ${minutes} : ${seconds}`);
-        this.clock_container?.appendChild(time);
-    }
+
+const showTime = () => {
+    let clock_container = document.getElementById('clock-container');
+    let date: Date = new Date();
+    let hours: number = date.getHours();
+    let minutes: number = date.getMinutes();
+    let seconds: number = date.getSeconds();
+    let time = document.createTextNode(`${hours} : ${minutes} : ${seconds}`);
+    clock_container?.appendChild(time);
 }
 
-let clock = new Clock();
-clock.showTime();
+showTime();
